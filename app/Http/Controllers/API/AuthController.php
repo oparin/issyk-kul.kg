48ed9e602874ff63a89b9f8208c6ab10ba6eef7c
kalyan.bratan <oparin85@gmail.com>
2025-01-27 18:39:16 +0600

    added login method in AuthController

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -9,6 +9,29 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends BaseController
 {
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'     => 'required|string|max:191',
+            'email'    => 'required|email|unique:users,email',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation error', $validator->errors(), 422);
+        }
+
+        $data = $validator->validated();
+
+        $user = User::query()->create([
+            'name'     => $data['name'],
+            'email'    => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        return $this->sendSuccess('Register successfully', $user);
+    }
+
     public function login(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -31,5 +54,11 @@ class AuthController extends BaseController
         $authToken = $user->createToken($data['email'])->plainTextToken;
 
         return $this->sendSuccess('Login successfully', ['token' => $authToken]);
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+
+        return $this->sendSuccess('Logged out');
     }
 }
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -7,7 +7,9 @@ use App\Http\Controllers\API\AuthController;
 Route::prefix('auth')->group(function () {
     Route::name('auth.')->group(function () {
         Route::controller(AuthController::class)->group(function () {
+            Route::post('/register', 'register')->name('register');
             Route::post('/login', 'login')->name('login');
+            Route::any('/logout','logout')->name('logout')->middleware('auth:sanctum');
         });
     });
 });
